a3d9d9e07820809f655aaa9e13afc7baab4195c1
Garril <zev20010120@163.com>
2023-02-18 17:22:35 +0800

    🏗 build(project): init garril-ui. add commitizen and config vuejsx

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <HelloWorld msg="Vite + Vue" />
+    <Test></Test>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test';
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
